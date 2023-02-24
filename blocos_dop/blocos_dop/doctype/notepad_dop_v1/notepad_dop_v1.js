efe8a3e55afcbda0b860ca30f10ca2a93b2255a1
hcesar <hcesar@gmail.com>
2023-02-24 15:04:37 -0800

    New Design of Doctype with all fields... instead of having 3 Doctypes for a single NotePad.

---
+++ blocos_dop/blocos_dop/doctype/notepad_dop_v1/notepad_dop_v1.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Helkyd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Notepad DOP_v1', {
+	// refresh: function(frm) {
+
+	// }
+});
